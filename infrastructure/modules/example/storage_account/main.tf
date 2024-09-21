14879f09dd654242ef41c923a3e7b77c56d9e112
John Collinson <13622412+johncollinson2001@users.noreply.github.com>
2024-09-21 22:02:23 +0100

    Removed example and redundant backup instance modules.

--- infrastructure/modules/example/storage_account/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
