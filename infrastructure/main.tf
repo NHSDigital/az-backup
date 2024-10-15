a4d43e39feb8ce966bf6d18ed0c5fc75e4c056ab
RAFIQ, Khurram (NHS DIGITAL) <khurram.rafiq@nhs.net>
2024-10-15 16:28:56 +0100

    added required azapi driver in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.3.0"
     }
+    azapi = {
+      source = "Azure/azapi"
+      version = "1.15.0"
+    }
   }
 
   backend "azurerm" {}
