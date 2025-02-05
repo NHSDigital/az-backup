636cb3a5cf88d126a8f8054bf311afeccab4442e
johncollinson2001 <13622412+johncollinson2001@users.noreply.github.com>
2025-02-05 16:44:57 +0000

    #34 added commentary to explain the slightly quirky behaviour of the resource group.

--- infrastructure/resource_group.tf
+++ infrastructure/resource_group.tf
@@ -1,3 +1,12 @@
+# The consumer of the module can configure whether the resource group should be created or
+# not. If the resource group should be created, the module will create it. If the resource 
+# group should not be created, the module will look up the existing resource group and 
+# reference it as data. 
+#
+# The local variable resource_group means the resource group - whether data or resource, 
+# can be easily referenced in other parts of the module.
+###########################################################################################
+
 resource "azurerm_resource_group" "resource_group" {
   count    = var.create_resource_group ? 1 : 0
   location = var.resource_group_location
